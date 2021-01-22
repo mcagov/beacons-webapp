91d895a782e41589eb43f8d3edd046eee0347b61
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-22 09:16:12 +0000

    Paste example GovUK accordion HTML to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,60 @@ import styles from "../styles/Home.module.scss";
 const Home: FunctionComponent = () => {
   return (
     <div className={styles.container}>
+      <div className="govuk-accordion" data-module="govuk-accordion" id="accordion-default">
+        <div className="govuk-accordion__section ">
+          <div className="govuk-accordion__section-header">
+            <h2 className="govuk-accordion__section-heading">
+        <span className="govuk-accordion__section-button" id="accordion-default-heading-1">
+          Writing well for the web
+        </span>
+            </h2>
+          </div>
+          <div id="accordion-default-content-1" className="govuk-accordion__section-content"
+               aria-labelledby="accordion-default-heading-1">
+            <p className='govuk-body'>This is the content for Writing well for the web.</p>
+          </div>
+        </div>
+        <div className="govuk-accordion__section ">
+          <div className="govuk-accordion__section-header">
+            <h2 className="govuk-accordion__section-heading">
+        <span className="govuk-accordion__section-button" id="accordion-default-heading-2">
+          Writing well for specialists
+        </span>
+            </h2>
+          </div>
+          <div id="accordion-default-content-2" className="govuk-accordion__section-content"
+               aria-labelledby="accordion-default-heading-2">
+            <p className='govuk-body'>This is the content for Writing well for specialists.</p>
+          </div>
+        </div>
+        <div className="govuk-accordion__section ">
+          <div className="govuk-accordion__section-header">
+            <h2 className="govuk-accordion__section-heading">
+        <span className="govuk-accordion__section-button" id="accordion-default-heading-3">
+          Know your audience
+        </span>
+            </h2>
+          </div>
+          <div id="accordion-default-content-3" className="govuk-accordion__section-content"
+               aria-labelledby="accordion-default-heading-3">
+            <p className='govuk-body'>This is the content for Know your audience.</p>
+          </div>
+        </div>
+        <div className="govuk-accordion__section ">
+          <div className="govuk-accordion__section-header">
+            <h2 className="govuk-accordion__section-heading">
+        <span className="govuk-accordion__section-button" id="accordion-default-heading-4">
+          How people read
+        </span>
+            </h2>
+          </div>
+          <div id="accordion-default-content-4" className="govuk-accordion__section-content"
+               aria-labelledby="accordion-default-heading-4">
+            <p className='govuk-body'>This is the content for How people read.</p>
+          </div>
+        </div>
+      </div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
