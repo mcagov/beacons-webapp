eea166fbf4d202adb73717659531b473479a079c
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-17 09:51:58 +0000

    Extract createMockFieldValidator function to clean up tests

--- test/lib/formValidator.test.ts
+++ test/lib/formValidator.test.ts
@@ -3,9 +3,15 @@ import { FieldValidator } from "../../src/lib/fieldValidator";
 
 describe("FormValidator", () => {
   describe("hasError()", () => {
-    beforeEach(() => {
+    const createMockFieldValidator = (fieldId, hasError) => {
       jest.mock("../../src/lib/fieldValidator");
-    });
+
+      const mockHasErrorMethod = jest.fn();
+      mockHasErrorMethod.mockReturnValue(hasError);
+      FieldValidator.prototype.hasError = mockHasErrorMethod;
+
+      return new FieldValidator(fieldId);
+    };
 
     it("should return false if there are no child fields", () => {
       const formWithNoFields = new FormValidator();
@@ -16,17 +22,29 @@ describe("FormValidator", () => {
     });
 
     it("should return false if none of the child fields contain an error", () => {
-      const mockHasError = jest.fn();
-      mockHasError.mockReturnValue(false);
-      FieldValidator.prototype.hasError = mockHasError;
+      const mockFieldValidatorWithNoError = createMockFieldValidator(
+        "fieldWithNoError",
+        false
+      );
 
-      const stubFieldWithNoError = new FieldValidator("fieldWithNoErrorId");
-
-      const formWithOneField = new FormValidator(stubFieldWithNoError);
+      const formWithOneField = new FormValidator(mockFieldValidatorWithNoError);
 
       const hasError = formWithOneField.hasError();
 
       expect(hasError).toBe(false);
+    });
+
+    it("should return true if one of the child fields contain an error", () => {
+      const mockFieldValidatorWithError = createMockFieldValidator(
+        "fieldWithOneError",
+        true
+      );
+
+      const formWithOneField = new FormValidator(mockFieldValidatorWithError);
+
+      const hasError = formWithOneField.hasError();
+
+      expect(hasError).toBe(true);
     });
   });
 });
