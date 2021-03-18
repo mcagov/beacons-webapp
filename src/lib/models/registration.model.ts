b744c70501b8d964145af647d952d718014ffcfd
Stuart Mindt <stuart.mindt@madetech.com>
2021-03-18 12:27:19 +0000

    models added for structured types

--- src/lib/models/registration.model.ts
+++ src/lib/models/registration.model.ts
@@ -1,28 +1,102 @@
-export class Registration {
-  constructor(registrationJson) {}
+import { Domain } from "node:domain";
 
-  public deserialize() {
-    return {
-      owner: {
-        name: "",
-        email: "",
-        telephoneNumber: "",
-        altTelephoneNumber: "",
-        address: {
-          line1: "",
-          line2: "",
-          townOrCity: "",
-          county: "",
-          postcode: "",
-        },
-      },
-      beacons: [],
-      emergencyContacts: [],
-    };
-  }
+// export class Registration {
+//   constructor(registrationJson) {}
 
-  public update(registrationJson) {
-    // TODO take flat object and update internal structured data
-    // E.g. user adds a beacon, create a new Beacon object and add to Registration
-  }
+//   public deserialize() {
+//     return {
+//       owner: {
+//         name: "",
+//         email: "",
+//         telephoneNumber: "",
+//         altTelephoneNumber: "",
+//         address: {
+//           line1: "",
+//           line2: "",
+//           townOrCity: "",
+//           county: "",
+//           postcode: "",
+//         },
+//       },
+//       beacons: [],
+//       emergencyContacts: [],
+//     };
+//   }
+
+//   public update(registrationJson) {
+//     // TODO take flat object and update internal structured data
+//     // E.g. user adds a beacon, create a new Beacon object and add to Registration
+//   }
+// }
+
+interface Registration {
+  beacons: Beacon[];
+  owner: Person;
+}
+
+interface Beacon {
+  manufacturer: string;
+  model: string;
+  hexId: string;
+  manufacturerSerialNumber: string;
+  uses: BeaconUse[];
+
+  chkCode: string;
+  batteryExpiryDate: string;
+  batteryExpiryDateMonth: string;
+  batteryExpiryDateYear: string;
+  lastServicedDate: string;
+  lastServicedDateMonth: string;
+  lastServicedDateYear: string;
+}
+
+interface BeaconUse {
+  domain: Domain;
+}
+
+interface Emplacement {
+  name: string;
+  maxCapacity: string;
+  baseLocation: string;
+  beaconLocation: string;
+  moreDetails: string;
+}
+
+interface Vessel extends Emplacement {
+  mmsiNumber: string;
+  pln: string;
+}
+
+interface Aircraft extends Emplacement {
+  manufacturer: string;
+}
+
+interface Vessel {
+  maxCapacity: string;
+  name: string;
+  homeport: string;
+  areaOfOperation: string;
+  beaconLocation: string;
+  moreVesselDetails: string;
+  maritimePleasureVesselUse: string;
+  otherPleasureVesselText: string;
+}
+
+interface Land {
+  name: string;
+}
+
+interface Other {
+  maxCapacity: string;
+  vesselName: string;
+  homeport: string;
+  areaOfOperation: string;
+  beaconLocation: string;
+  moreVesselDetails: string;
+  maritimePleasureVesselUse: string;
+  otherPleasureVesselText: string;
+}
+
+interface Person {
+  name: string;
 }
