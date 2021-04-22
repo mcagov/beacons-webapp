a415466f47c5b29783489bb1e1d13d529a10bc64
Matthew Carr <matt.carr@madetech.com>
2021-04-22 10:34:17 +0100

    Remove contract tests for now

--- jest.contract.config.ts
+++
@@ -1,9 +0,0 @@
-// See: https://jestjs.io/docs/en/configuration for a full list of configuration options
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/cypress", "/test"],
-  testEnvironment: "node",
-};
-
-export default config;
